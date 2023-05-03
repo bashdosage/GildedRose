a6505b05110870a439748d71905e9358aaeec4eb
Juan T. Garcia <juantomasg@yahoo.com>
2023-05-03 17:35:55 -0400

    test: add tests for Elixir of the Mongoose

--- test/vitest/gilded-rose.spec.ts
+++ test/vitest/gilded-rose.spec.ts
@@ -46,3 +46,25 @@ describe('When adding a new Aged Brie item to Gilded Rose', () => {
     });
   });
 });
+
+describe('When adding a new "Elixir of the Mongoose" item to Gilded Rose', () => {
+  describe('with initial SellIn value of 5 days and Quality value of 7', () => {
+    let gildedRose;
+
+    beforeEach(() => {
+      // ARRANGE
+      gildedRose = new GildedRose([new Item('Elixir of the Mongoose', 5, 7)]);
+    });
+
+    describe('and one day passes', () => {
+      it('should decrease the quality and SellIn date', () => {
+        // ACT
+        const updatedItems = gildedRose.updateQuality();
+
+        // ASSERT
+        expect(updatedItems[0].sellIn).toEqual(4);
+        expect(updatedItems[0].quality).toEqual(6);
+      });
+    });
+  });
+});
