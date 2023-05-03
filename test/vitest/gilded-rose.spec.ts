7a8c6229346f91f4284fef90f97255383d190b3c
Juan T. Garcia <juantomasg@yahoo.com>
2023-05-03 17:29:45 -0400

    test: add tests for Aged Brie

--- test/vitest/gilded-rose.spec.ts
+++ test/vitest/gilded-rose.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
@@ -6,5 +6,43 @@ describe('Gilded Rose', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
+  });
+});
+
+describe('When adding a new Aged Brie item to Gilded Rose', () => {
+  describe('with initial SellIn value of 2 days and Quality value of 0', () => {
+    let gildedRose;
+
+    beforeEach(() => {
+      // ARRANGE
+      gildedRose = new GildedRose([new Item('Aged Brie', 2, 0)]);
+    });
+
+    describe('and one day passes', () => {
+      it('should increase the quality to 1 and decrease the SellIn date to 1', () => {
+        // ACT
+        const updatedItems = gildedRose.updateQuality();
+
+        // ASSERT
+        expect(updatedItems[0].sellIn).toEqual(1);
+        expect(updatedItems[0].quality).toEqual(1);
+      });
+    });
+
+    describe('and two days pass', () => {
+      it('should increase the quality to 2 and decrease the SellIn date to 0', () => {
+        const days = 2;
+        let updatedItems;
+
+        // ACT
+        for (let i = 0; i < days; i++) {
+          updatedItems = gildedRose.updateQuality();
+        }
+
+        // ASSERT
+        expect(updatedItems[0].sellIn).toEqual(0);
+        expect(updatedItems[0].quality).toEqual(2);
+      });
+    });
   });
 });
